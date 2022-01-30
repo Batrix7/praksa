227e63ad428ac6fb8dbe64e5432ec159e47d0ceb
Batrix ★ <92871807+Batrix7@users.noreply.github.com>
2022-01-30 18:49:01 +0100

    Update vaja7.2.py

--- vaja7.2.py
+++ vaja7.2.py
@@ -1,1 +1,15 @@
-prevozeni_kilometri ?
+u = int(input("Vnesi stevilo ur: "))
+m = int(input("Vnesi stevilo minut: "))
+
+if m > 30:
+    st1 = 60 - m
+    u = u + 1
+    print("Ura je " + str(st1) + " minut do " + str(u))
+elif 30 > m > 1:
+    print("Ura je " + str(m) + " minut cez " + str(u))
+elif m == 30:
+    u = u + 1
+    print("Ura je pol " + str(u))
+if m == 00:
+    print("Ura je " + str(u))
+
